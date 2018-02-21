3f9fd2d2789eb11e6378ccc453814d9b62aa487a
fond <gregorysallen.e@gmail.com>
2018-02-21 16:16:55 +0100

    modif views

--- mdrapp/views.py
+++ mdrapp/views.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 
-#sof = load_data_csv(app.config['ENGINE_DATABASE'])
+sof = load_data_csv(app.config['ENGINE_DATABASE'])
 
 @app.route('/')
 @app.route('/index/')
@@ -17,13 +17,13 @@ def index():
     return render_template('index.html' )
 
 
-#@app.route('/film/<id_film>')
-#def film(id_film):
-#	title = sof.get_title_from_id(int(id_film))
-#	return render_template('film.html', id_film = id_film, title_film = title)
+@app.route('/film/<id_film>')
+def film(id_film):
+	title = sof.get_title_from_id(int(id_film))
+	return render_template('film.html', id_film = id_film, title_film = title)
 
-#@app.route('/film/')
-#def film2():
-#	idf = request.args.get('id_film')
-#	title = sof.get_title_from_id(int(idf))
-#	return render_template('film.html', id_film = idf, title_film = title)
+@app.route('/film/')
+def film2():
+	idf = request.args.get('id_film')
+	title = sof.get_title_from_id(int(idf))
+	return render_template('film.html', id_film = idf, title_film = title)
